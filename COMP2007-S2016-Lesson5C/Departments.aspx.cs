73f27e47056e4872002b685982842bc25cd2566f
MikeMeissner <tennisapb@gmail.com>
2016-06-13 16:38:07 -0400

    Add project files.

---
+++ COMP2007-S2016-Lesson5C/Departments.aspx.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// using statements that are required to connect to EF DB
+using COMP2007_S2016_Lesson5C.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
+namespace COMP2007_S2016_Lesson5C
+{
+    public partial class Departments : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // if loading the page for the first time, populate the department grid
+            if (!IsPostBack)
+            {
+                Session["SortColumn"] = "DepartmentID"; // default sort column
+                Session["SortDirection"] = "ASC";
+                // Get the student data
+                this.GetDepartments();
+            }
+        }
+        protected void GetDepartments()
+        {
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                string SortString = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+
+                // query the Students Table using EF and LINQ
+                var Departments = (from allDepartments in db.Departments
+                                select allDepartments);
+
+                // bind the result to the GridView
+                DepartmentsGridView.DataSource = Departments.AsQueryable().OrderBy(SortString).ToList();
+                DepartmentsGridView.DataBind();
+            }
+        }
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Set the new Page size
+            DepartmentsGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            // refresh the grid
+            this.GetDepartments();
+        }
+
+        protected void DepartmentsGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            // store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            // get the selected DepartmentID using the Grid's DataKey collection
+            int DepartmentID = Convert.ToInt32(DepartmentsGridView.DataKeys[selectedRow].Values["DepartmentID"]);
+
+            // use EF to find the selected student in the DB and remove it
+            using (DefaultConnection db = new DefaultConnection())
+            {
+                // create object of the Student class and store the query string inside of it
+                Department deletedDepartment = (from departmentRecords in db.Departments
+                                          where departmentRecords.DepartmentID == DepartmentID
+                                          select departmentRecords).FirstOrDefault();
+
+                // remove the selected student from the db
+                db.Departments.Remove(deletedDepartment);
+
+                // save my changes back to the database
+                db.SaveChanges();
+
+                // refresh the grid
+                this.GetDepartments();
+            }
+        }
+
+        protected void DepartmentsGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            // Set the new page number
+            DepartmentsGridView.PageIndex = e.NewPageIndex;
+
+            // refresh the grid
+            this.GetDepartments();
+        }
+
+        protected void DepartmentsGridView_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            // get the column to sorty by
+            Session["SortColumn"] = e.SortExpression;
+
+            // Refresh the Grid
+            this.GetDepartments();
+
+            // toggle the direction
+            Session["SortDirection"] = Session["SortDirection"].ToString() == "ASC" ? "DESC" : "ASC";
+        }
+
+        protected void DepartmentsGridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ COMP2007-S2016-Lesson5C/DepartmentsDetails.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DepartmentsDetails.aspx.cs" Inherits="COMP2007_S2016_Lesson5C.DepartmentsDetails" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <h1>Department Details</h1>
+                <h5>All Fields are Required</h5>
+                <br />
+                <div class="form-group">
+                    <label class="control-label" for="DepartmentNameTextBox">Department Name</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="DepartmenrNameTextBox" placeholder="Department Name" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="DepartmentIDTextBox">Department ID</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="DepartmentIDTextBox" placeholder="Department ID" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="BudgetTextBox">Budget</label>
+                    <asp:TextBox runat="server" TextMode="Date" CssClass="form-control" ID="BudgetTextBox" required="true"></asp:TextBox>
+                    
+                </div>
+                <div class="text-right">
+                    <asp:Button Text="Cancel" ID="CancelButton" CssClass="btn btn-warning btn-lg" runat="server" 
+                        UseSubmitBehavior="false" CausesValidation="false" OnClick="CancelButton_Click" />
+                    <asp:Button Text="Save" ID="SaveButton" CssClass="btn btn-primary btn-lg" runat="server" OnClick="SaveButton_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
